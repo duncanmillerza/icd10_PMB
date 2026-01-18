63f090d4e7786b8f131915ec11ae9e579d0466c2
duncanmillerza <duncanmillerza@gmail.com>
2026-01-18 20:23:43 +0200

    Fix: manifest icon purpose type error

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,7 +14,7 @@ export default function manifest(): MetadataRoute.Manifest {
                 src: '/Mobile Icon.svg',
                 sizes: 'any',
                 type: 'image/svg+xml',
-                purpose: 'any maskable',
+                purpose: 'maskable',
             },
             {
                 src: '/favicon.ico',
